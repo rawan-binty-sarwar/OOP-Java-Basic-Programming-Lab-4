d200d97303a20d2695d10b7e61e0bdbbc2b90e7d
rawan-binty-sarwar <rawanbintysarwar4170@gmail.com>
2026-08-22 19:54:41 +0600

    Add ElseExample1 class with rain check logic

---
+++ ElseExample.java
@@ -0,0 +1,10 @@
+public class ElseExample1 {
+    public static void main(String[] args) {
+        boolean isRaining = false;
+        if (isRaining) {
+            System.out.println("Bring an umbrella!");
+        } else {
+            System.out.println("No rain today, no need for an umbrella!");
+        }
+    }
+}
